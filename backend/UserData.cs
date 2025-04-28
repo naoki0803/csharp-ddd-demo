351d877b633c7b48f1531b58e57cd2769f5bc3a0
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2025-04-28 12:37:55 +0900

    新しいUserDataクラスを追加し、UserインスタンスからIDと名前を取得する構造を実装。

---
+++ backend/UserData.cs
@@ -0,0 +1,18 @@
+﻿namespace TodoApi;
+
+// パラメーターを個別に受け取るパターン
+// public class UserData(string id, string name)
+// {
+//     public string Id { get; } = id;
+//     public string Name { get; } = name;
+// }
+
+// User 自体を受け取るパターン
+public class UserData(User source)
+{
+    public string Id { get; } = source.Id.Value;
+    public string Name { get; } = source.Name.Value;
+
+    // 仮にパラメーターが増えた場合以下追加をする事で変換が容易になる。
+    // public string Age { get; } = source.Age.Value;
+}

454e174176f7b7f31e442f7e388f12382310293b
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2025-04-29 06:25:16 +0900

    ユーザー情報更新のためのコマンドオブジェクトUserUpdateCommandを新規作成

---
+++ backend/Applications/Users/UserUpdateCommand.cs
@@ -0,0 +1,25 @@
+﻿namespace TodoApi;
+
+public class UserUpdateCommand
+{
+    public UserUpdateCommand(string id)
+    {
+        Id = id;
+    }
+    public string Id { get; }
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+
+    // 以下のように、コンストラクタの引数で id 以外を null で表現してもよい
+    // public UserUpdateCommand(string id, string name = null, string email = null)
+    // {
+    //     Id = id;
+    //     Name = name;
+    //     Email = email;
+    // }
+
+    // public string Id { get; }
+    // public string Name { get; }
+    // public string Email { get; }
+
+}

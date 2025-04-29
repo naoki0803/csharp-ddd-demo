741e5945da6e0b027e5ef4ed7056a72813b8907e
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2025-04-30 05:52:20 +0900

    ユーザー削除サービスを新規作成し

---
+++ backend/Applications/Services/UserDeleteService.cs
@@ -0,0 +1,25 @@
+﻿namespace TodoApi;
+
+public class UserDeleteService
+{
+    private readonly IUserRepository _userRepository;
+
+    public UserDeleteService(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+
+    public async Task<UserData?> Handle(UserDeleteCommand command)
+    {
+        var targetId = new UserId(command.Id);
+        var user = await _userRepository.Find(targetId);
+        if (user == null)
+        {
+            throw new Exception("ユーザーが見つかりません。");
+        }
+
+        await _userRepository.Delete(user);
+
+        return new UserData(user);
+    }
+}

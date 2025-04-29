7ddc52fabc14bb0b75b340b2cd23b14138db8d37
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2025-04-30 05:53:01 +0900

    ユーザー情報更新のためのUserUpdateServiceクラスを新規作成し、ユーザー名の変更処理を実装。

---
+++ backend/Applications/Services/UserUpdateService.cs
@@ -0,0 +1,39 @@
+﻿namespace TodoApi;
+
+public class UserUpdateService
+{
+    private readonly IUserRepository _userRepository;
+    private readonly UserService _userService;
+
+    public UserUpdateService(IUserRepository userRepository, UserService userService)
+    {
+        _userRepository = userRepository;
+        _userService = userService;
+    }
+
+    public async Task<UserData?> Handle(UserUpdateCommand command)
+    {
+        var targetId = new UserId(command.Id);
+        var user = await _userRepository.Find(targetId);
+        if (user == null)
+        {
+            return null;
+        }
+        var name = command.Name;
+
+        if (name != null)
+        {
+            var newUserName = new UserName(name);
+            if (await _userService.Exists(newUserName))
+            {
+                throw new Exception("ユーザーが既に存在します。");
+            }
+            user.ChangeName(name);
+        }
+
+        await _userRepository.Save(user);
+
+        var userData = new UserData(user);
+        return userData;
+    }
+}

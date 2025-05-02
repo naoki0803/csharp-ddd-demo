36872fa64aaafb97d97d467b825a058d6debfbc8
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2025-05-02 23:13:42 +0900

    ユーザー登録機能を提供するUserControllerクラスを新規作成し、UserApplicationServiceを通じてユーザー登録コマンドを処理するPOSTメソッドを実装

---
+++ backend/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TodoApi;
+
+[Route("api/Controller")]
+public class UserController : ControllerBase
+{
+    private readonly UserApplicationService _userApplicationService;
+
+    public UserController(UserApplicationService userApplicationService)
+    {
+        _userApplicationService = userApplicationService;
+    }
+
+    [HttpPost]
+    public void Post([FromBody] UserPostRequestModel request)
+    {
+        var command = new UserRegisterCommand(request.Name);
+        _userApplicationService.Register(command);
+    }
+
+}

754b32d5ceafa26f200a46b62d4ecee33f85e287
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2025-05-04 07:18:40 +0900

    コントローラーに、ユーザー一覧取得のGETメソッドを追加

--- backend/UserController.cs
+++ backend/UserController.cs
@@ -5,18 +5,38 @@ namespace TodoApi;
 [Route("api/Controller")]
 public class UserController : ControllerBase
 {
-    private readonly UserApplicationService _userApplicationService;
+    private readonly UserRegisterService _registerService;
+    private readonly UserGetService _getService;
+    private readonly UserGetAllService _getAllService;
+    private readonly UserUpdateService _updateService;
+    private readonly UserDeleteService _deleteService;
 
-    public UserController(UserApplicationService userApplicationService)
+    public UserController(
+        UserRegisterService registerService,
+        UserGetService getService,
+        UserGetAllService getAllService,
+        UserUpdateService updateService,
+        UserDeleteService deleteService)
     {
-        _userApplicationService = userApplicationService;
+        _registerService = registerService;
+        _getService = getService;
+        _getAllService = getAllService;
+        _updateService = updateService;
+        _deleteService = deleteService;
     }
 
     [HttpPost]
     public void Post([FromBody] UserPostRequestModel request)
     {
         var command = new UserRegisterCommand(request.Name);
-        _userApplicationService.Register(command.Name);
+        _registerService.Handle(command);
     }
 
+    [HttpGet]
+    public async Task<List<UserIndexResponseModel>> Index()
+    {
+        var result = await _getAllService.Handle();
+        var users = result.Select(user => new UserIndexResponseModel(user.Id, user.Name)).ToList();
+        return users;
+    }
 }

21cbbbf84553d5bd16308acbbc62133bfdd9692e
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2025-05-04 07:20:06 +0900

    ユーザー一覧を取得する新しいサービスUserGetAllServiceを追加

---
+++ backend/Applications/Users/Services/UserGetAllService.cs
@@ -0,0 +1,23 @@
+﻿namespace TodoApi;
+
+public class UserGetAllService
+{
+    private readonly IUserRepository _userRepository;
+
+    public UserGetAllService(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+
+    public async Task<List<UserData>?> Handle()
+    {
+        var users = await _userRepository.FindAll();
+        if (users == null)
+        {
+            // throw new Exception("ユーザーが見つかりません。");
+            return null;
+        }
+        var usersData = users.Select(user => new UserData(user)).ToList();
+        return usersData;
+    }
+}

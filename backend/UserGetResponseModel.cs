67b1db749c65204dcf6a01431bab125a00313a0b
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2025-05-05 06:22:54 +0900

    ユーザー情報を取得するためのレスポンスモデルUserGetResponseModelクラスを新規作成

---
+++ backend/UserGetResponseModel.cs
@@ -0,0 +1,12 @@
+﻿namespace TodoApi;
+
+public class UserGetResponseModel
+{
+    public UserGetResponseModel(string id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+    public string Id { get; }
+    public string Name { get; }
+}
